b3c00c5c573dc2067bb50e2fdd162caa51e288f3
Denys Sichka <128291891+alienqqe@users.noreply.github.com>
2025-10-12 14:05:39 +0300

    Add membership type for the 10 entries membership

--- hellofx/src/main/java/org/openjfx/hellofx/entities/Membership.java
+++ hellofx/src/main/java/org/openjfx/hellofx/entities/Membership.java
@@ -7,6 +7,6 @@ import java.time.LocalDate;
 public record Membership(Long id, LocalDate startDate, LocalDate expiresAt, double price, MembershipType Type, Long idOfHolder ){
 
     public enum MembershipType{
-        Monthly, Yearly, Weekly
+        Monthly, Yearly, Weekly, 10
     }
 }
